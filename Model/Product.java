f6c3a935ad0f4f907502f70a96f73ed10cb12b92
Ismail Ercedogan <ismail.ercedogan@ozu.edu.tr>
2021-05-15 10:39:39 +0300

    Product class is generated and its functions implemented

---
+++ Model/Product.java
@@ -0,0 +1,38 @@
+package Model;
+
+public abstract class Product implements Expense{
+private String name;
+private double purchasePrice;
+private double sellingPrice;
+private double utilityCost;
+public Product(String name,double purchasePrice,double sellingPrice,double utilityCost) {
+	this.name=name;
+	this.purchasePrice=purchasePrice;
+	this.sellingPrice=sellingPrice;
+	this.utilityCost=utilityCost;
+}
+public Product(String name) {
+	this.name=name;
+}
+public String getName() {
+	return this.name;
+}
+public void setName(String name) {
+	this.name=name;
+}
+public double getPuchasePrice() {
+	return this.purchasePrice;
+}
+public double getSellingPrice() {
+	return this.sellingPrice;
+}
+public void setSellingPrice(double sellingPrice) {
+	this.sellingPrice=sellingPrice;
+}
+public double getUtilityCost() {
+	return this.utilityCost;
+}
+public String toString() {
+	return "";
+}
+}
