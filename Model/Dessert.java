bfb2536d5025d3e1a9a754751646411ad068173a
Ismail Ercedogan <ismail.ercedogan@ozu.edu.tr>
2021-05-15 10:49:22 +0300

    Dessert Class is done

---
+++ Model/Dessert.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Dessert extends Product{
+	public Dessert(String name,double purchasePrice,double sellingPrice,double utilityCost) {
+		super(name,purchasePrice,sellingPrice,utilityCost);
+	}
+	public double calculateExpense() {
+		return super.getPuchasePrice()+super.getUtilityCost();
+	}
+}
