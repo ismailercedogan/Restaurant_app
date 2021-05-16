11a1c0844d51d652ea51f818d6c860261b8d8cd2
Ismail Ercedogan <ismail.ercedogan@ozu.edu.tr>
2021-05-16 06:48:43 +0300

    MenuProduct class has been added as a subclass of Product

---
+++ Model/MenuProduct.java
@@ -0,0 +1,40 @@
+package Model;
+import java.util.ArrayList;
+
+public class MenuProduct extends Product{
+private ArrayList<Product> products=new ArrayList<>();
+	public MenuProduct(String name,ArrayList <Product> products) {
+	super(name);
+	this.products=products;
+}
+public double calculateExpense() {
+	double totalExpense=0.0;
+	for(Product product:this.products) {
+		if(product instanceof MainDish) {
+			totalExpense+=((MainDish)product).calculateExpense();
+		}
+		if(product instanceof Dessert) {
+			totalExpense+=((Dessert)product).calculateExpense();
+		}	
+		if(product instanceof Beverage) {
+			totalExpense+=((Beverage)product).calculateExpense();
+		}
+	}
+return totalExpense;
+}
+public double getSellingPrice() {
+	double totalPrice=0.0;
+	for(Product product:this.products) {
+		if(product instanceof MainDish) {
+			totalPrice+=(((MainDish)product).calculateExpense())-(((MainDish)product).calculateExpense()*0.1);
+		}
+		if(product instanceof Dessert) {
+			totalPrice+=(((Dessert)product).calculateExpense())-(((Dessert)product).calculateExpense()*0.2);
+		}	
+		if(product instanceof Beverage) {
+			totalPrice+=(((Beverage)product).calculateExpense())-(((Beverage)product).calculateExpense())*0.5;
+		}
+	}
+return totalPrice;
+}
+}
